06382e8f5f80266f3d365c642ce5d0881ec8c847
VolodymyrOsinchuk <osinchuk28@live.com>
2021-08-05 12:23:24 +0200

    install depencies

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const {Client} = require("pg");
 
 const app = express();
+
+// DB postgress connection
+//const client =  new Client();
+//client.connect();
+
+//const res = client.query(`SELECT $1:: text as message`, ['Hello world']);
+//console.log("res >>>>",res[0]);
+//client.end();
+
 
 app.get("/", (req, res) => {
   res.send("Hello from qovery");
 });
 
 app.get("/api", (req, res) => {
-  res.json({ message: "API qovery" });  
+  res.json({ message: "API qovery" });
 });
+
+app.use(express.static("public"));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8000;
 
